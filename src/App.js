98e00fd25033a141825851e30f74456eb371e014
bajatin <jatinbaghel2000@gmail.com>
2021-07-18 16:56:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core';
+import { Book }  from '@material-ui/icons';
+
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+
+import useStyles from './ui-albums/styles';
+
+const cards = [1,2,3,4,5,6,7,8,9];
+
+const App = ()  => { 
+  const classes = useStyles();
+  return (
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <Book className={classes.icon}/>
+          <Typography variant="h6">
+            E-booker
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div className={classes.container}>
+          <Container maxWidth="sm">
+            {/* this is the header */}
+            <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+                E-Booker
+            </Typography>
+            {/* this will be the description */}
+            <Typography variant="h5" align="center" color="textSecondary" paragraph>
+              Come explore the world of books. Browse through the internet and choose your favorites from millions books. Read your offline epubs online
+            </Typography>
+            <div className={classes.button}> 
+                <Grid container spacing={2} justify="center">
+                  <Grid item>
+                  <ButtonGroup variant="text" color="primary" aria-label="text primary button group">
+                    <Button>My Books</Button>
+                    <Button>Browse</Button>
+                  </ButtonGroup>
+                  </Grid>
+                </Grid>
+            </div>
+          </Container>
+        </div>
+         <Container className={classes.cardGrid} maxWidth="md">
+           <Grid container spacing={4}>
+             {cards.map((card) => (
+                <Grid item key={ card } xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia  
+                  className={classes.cardMedia} 
+                  image="https://source.unsplash.com/random"
+                  title="Image Title"
+                  />
+                  <CardContent className={classes.CardContent}>
+                    <Typography gutterBottom variant="h5">
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can describe the card here
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">Details</Button>
+                    <Button size="small" color="primary">Read</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+             ))}
+             
+           </Grid>
+         </Container>
+      </main>
+      <footer className={classes.footer}>
+              <Typography variaint='h6' align='center' gutterBottom>
+                Footer
+              </Typography>
+              <Typography variant='subtitle1' align='center' color='textSecondary'>
+                Something here to give footer a purpose
+              </Typography>
+      </footer>
+    </>
+  )
+}
+
+export default App;
